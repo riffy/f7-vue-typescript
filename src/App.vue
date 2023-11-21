64f18c78d43609bb581f38fb050126b164527c91
Meisterboom <lol@lol.de>
2023-11-21 14:10:30 +0100

    linter, facing decorator, config

--- src/App.vue
+++ src/App.vue
@@ -1,173 +1,227 @@
 <template>
-  <f7-app v-bind="f7params">
-    <!-- Left panel with cover effect-->
-    <f7-panel left cover dark>
-      <f7-view>
-        <f7-page>
-          <f7-navbar title="Left Panel"></f7-navbar>
-          <f7-block>Left panel content goes here</f7-block>
-        </f7-page>
-      </f7-view>
-    </f7-panel>
+	<f7App v-bind="f7params">
+		<!-- Left panel with cover effect-->
+		<f7Panel
+			left
+			cover
+			dark
+		>
+			<f7View>
+				<f7Page>
+					<f7Navbar title="Left Panel" />
+					<f7Block>Left panel content goes here</f7Block>
+				</f7Page>
+			</f7View>
+		</f7Panel>
 
-    <!-- Right panel with reveal effect-->
-    <f7-panel right reveal dark>
-      <f7-view>
-        <f7-page>
-          <f7-navbar title="Right Panel"></f7-navbar>
-          <f7-block>Right panel content goes here</f7-block>
-        </f7-page>
-      </f7-view>
-    </f7-panel>
+		<!-- Right panel with reveal effect-->
+		<f7Panel
+			right
+			reveal
+			dark
+		>
+			<f7View>
+				<f7Page>
+					<f7Navbar title="Right Panel" />
+					<f7Block>Right panel content goes here</f7Block>
+				</f7Page>
+			</f7View>
+		</f7Panel>
 
-    <!-- Views/Tabs container -->
-    <f7-views tabs class="safe-areas">
-      <!-- Tabbar for switching views-tabs -->
-      <f7-toolbar tabbar labels bottom>
-        <f7-link tab-link="#view-home" tab-link-active>
-          <i class="i-tabler-home w-6 h-6" />
-          <span class="normal-case text-sm">Home</span>
-        </f7-link>
-        <f7-link tab-link="#view-about">
-          <i class="i-tabler-user w-6 h-6" />
-          <span class="normal-case text-sm">About</span>
-        </f7-link>
-        <f7-link tab-link="#view-settings">
-          <i class="i-tabler-settings w-6 h-6" />
-          <span class="normal-case text-sm">Settings</span>
-        </f7-link>
-      </f7-toolbar>
+		<!-- Views/Tabs container -->
+		<f7Views
+			tabs
+			class="safe-areas"
+		>
+			<!-- Tabbar for switching views-tabs -->
+			<f7Toolbar
+				tabbar
+				labels
+				bottom
+			>
+				<f7Link
+					tab-link="#view-home"
+					tab-link-active
+				>
+					<i class="i-tabler-home w-6 h-6" />
+					<span class="normal-case text-sm">Home</span>
+				</f7Link>
+				<f7Link tab-link="#view-about">
+					<i class="i-tabler-user w-6 h-6" />
+					<span class="normal-case text-sm">About</span>
+				</f7Link>
+				<f7Link tab-link="#view-settings">
+					<i class="i-tabler-settings w-6 h-6" />
+					<span class="normal-case text-sm">Settings</span>
+				</f7Link>
+			</f7Toolbar>
 
-      <!-- Your main view/tab, should have "view-main" class. It also has "tab-active" class -->
-      <f7-view id="view-home" main tab tab-active url="/"></f7-view>
+			<!-- Your main view/tab, should have "view-main" class. It also has "tab-active" class -->
+			<f7View
+				id="view-home"
+				main
+				tab
+				tab-active
+				url="/"
+			/>
 
-      <!-- About View -->
-      <f7-view id="view-about" name="about" tab url="/about/"></f7-view>
+			<!-- About View -->
+			<f7View
+				id="view-about"
+				name="about"
+				tab
+				url="/about/"
+			/>
 
-      <!-- Settings View -->
-      <f7-view
-        id="view-settings"
-        name="settings"
-        tab
-        url="/settings/"
-      ></f7-view>
-    </f7-views>
+			<!-- Settings View -->
+			<f7View
+				id="view-settings"
+				name="settings"
+				tab
+				url="/settings/"
+			/>
+		</f7Views>
 
-    <!-- Popup -->
-    <f7-popup id="my-popup">
-      <f7-view>
-        <f7-page>
-          <f7-navbar title="Popup">
-            <f7-nav-right>
-              <f7-link popup-close>Close</f7-link>
-            </f7-nav-right>
-          </f7-navbar>
-          <f7-block>
-            <p>Popup content goes here.</p>
-          </f7-block>
-        </f7-page>
-      </f7-view>
-    </f7-popup>
+		<!-- Popup -->
+		<f7Popup id="my-popup">
+			<f7View>
+				<f7Page>
+					<f7Navbar title="Popup">
+						<f7NavRight>
+							<f7Link popup-close>
+								Close
+							</f7Link>
+						</f7NavRight>
+					</f7Navbar>
+					<f7Block>
+						<p>Popup content goes here.</p>
+					</f7Block>
+				</f7Page>
+			</f7View>
+		</f7Popup>
 
-    <f7-login-screen id="my-login-screen">
-      <f7-view>
-        <f7-page login-screen>
-          <f7-login-screen-title>Login</f7-login-screen-title>
-          <f7-list form>
-            <f7-list-input
-              type="text"
-              name="username"
-              placeholder="Your username"
-              v-model:value="username"
-            ></f7-list-input>
-            <f7-list-input
-              type="password"
-              name="password"
-              placeholder="Your password"
-              v-model:value="password"
-            ></f7-list-input>
-          </f7-list>
-          <f7-list>
-            <f7-list-button
-              title="Sign In"
-              @click="alertLoginData"
-            ></f7-list-button>
-            <f7-block-footer>
-              Some text about login information.<br />Click "Sign In" to close
-              Login Screen
-            </f7-block-footer>
-          </f7-list>
-        </f7-page>
-      </f7-view>
-    </f7-login-screen>
-  </f7-app>
+		<f7LoginScreen id="my-login-screen">
+			<f7View>
+				<f7Page login-screen>
+					<f7LoginScreenTitle>Login</f7LoginScreenTitle>
+					<f7List form>
+						<f7ListInput
+							v-model:value="username"
+							type="text"
+							name="username"
+							placeholder="Your username"
+						/>
+						<f7ListInput
+							v-model:value="password"
+							type="password"
+							name="password"
+							placeholder="Your password"
+						/>
+					</f7List>
+					<f7List>
+						<f7ListButton
+							title="Sign In"
+							@click="alertLoginData"
+						/>
+						<f7BlockFooter>
+							Some text about login information.<br>Click "Sign In" to close
+							Login Screen
+						</f7BlockFooter>
+					</f7List>
+				</f7Page>
+			</f7View>
+		</f7LoginScreen>
+	</f7App>
 </template>
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
+<script lang="ts">
 import {
-  f7,
-  f7ready,
-  f7App,
-  f7Page,
-  f7View,
-  f7Panel,
-  f7Block,
-  f7BlockFooter,
-  f7List,
-  f7LoginScreen,
-  f7LoginScreenTitle,
-  f7Link,
-  f7Navbar,
-  f7NavRight,
-  f7Toolbar,
-  f7Views,
-  f7Popup,
-  f7ListInput,
-  f7ListButton,
+	f7,
+	f7ready,
+	f7App,
+	f7Page,
+	f7View,
+	f7Panel,
+	f7Block,
+	f7BlockFooter,
+	f7List,
+	f7LoginScreen,
+	f7LoginScreenTitle,
+	f7Link,
+	f7Navbar,
+	f7NavRight,
+	f7Toolbar,
+	f7Views,
+	f7Popup,
+	f7ListInput,
+	f7ListButton,
 } from "framework7-vue";
 import { getDevice } from "framework7/lite-bundle";
 import capacitorApp from "./capacitor-app";
 import routes from "./routes/router";
+import { Vue, Component } from "vue-facing-decorator";
 
-const device = getDevice();
-// Framework7 Parameters
-const f7params = {
-  name: "F7-Vue Typescript", // App name
-  theme: "auto", // Automatic theme detection
-  id: "com.example.app", // App bundle ID
-  // App routes
-  routes: routes,
+@Component({
+	components: {
+		f7App,
+		f7Navbar,
+		f7Page,
+		f7Panel,
+		f7Views,
+		f7View,
+		f7Block,
+		f7BlockFooter,
+		f7List,
+		f7ListInput,
+		f7ListButton,
+		f7Popup,
+		f7LoginScreen,
+		f7LoginScreenTitle,
+		f7Toolbar,
+		f7Link,
+		f7NavRight
+	}
+})
+export default class App extends Vue {
+	device = getDevice();
+	f7params = {
+		name: "F7-Vue Typescript", // App name
+		theme: "auto", // Automatic theme detection
+		id: "com.example.app", // App bundle ID
+		// App routes
+		routes: routes,
 
-  // Input settings
-  input: {
-    scrollIntoViewOnFocus: device.capacitor,
-    scrollIntoViewCentered: device.capacitor,
-  },
-  // Capacitor Statusbar settings
-  statusbar: {
-    iosOverlaysWebView: true,
-    androidOverlaysWebView: false,
-  },
-};
-// Login screen data
-const username = ref("");
-const password = ref("");
+		// Input settings
+		input: {
+			scrollIntoViewOnFocus: this.device.capacitor,
+			scrollIntoViewCentered: this.device.capacitor,
+		},
+		// Capacitor Statusbar settings
+		statusbar: {
+			iosOverlaysWebView: true,
+			androidOverlaysWebView: false,
+		},
+	};
+	username = "";
+	password = "";
 
-const alertLoginData = () => {
-  f7.dialog.alert(
-    "Username: " + username.value + "<br>Password: " + password.value,
-    () => {
-      f7.loginScreen.close();
-    }
-  );
-};
-onMounted(() => {
-  f7ready(() => {
-    // Init capacitor APIs (see capacitor-app.js)
-    if (device.capacitor) {
-      capacitorApp.init(f7);
-    }
-    // Call F7 APIs here
-  });
-});
+	alertLoginData() {
+		f7.dialog.alert(
+			"Username: " + this.username + "<br>Password: " + this.password,
+			() => {
+				f7.loginScreen.close();
+			}
+		);
+	}
+
+	mounted() {
+		f7ready(() => {
+			// Init capacitor APIs (see capacitor-app.js)
+			if (this.device.capacitor) {
+				capacitorApp.init(f7);
+			}
+			// Call F7 APIs here
+			console.log(this.device);
+		});
+	}
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 import i18n from "./locale";
 import Framework7 from "framework7/lite-bundle";
-import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
-import App from './App.vue'
+import Framework7Vue, { registerComponents } from "framework7-vue/bundle";
+import App from "./App.vue";
 
 
 import "framework7/css/bundle";
-import 'uno.css'
-import './app.css';
+import "uno.css";
+import "./app.css";
 
 // Init Framework7-Vue Plugin
 Framework7.use(Framework7Vue);
 
-const app = createApp(App)
+const app = createApp(App);
 
 // Register All Framework7 Vue components
 // registerComponents(app);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.use(i18n)
+app.use(i18n);
 
-app.mount('#app')
+app.mount("#app");
